889d783881dfca297a60b116ac45ea46266b7fb9
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-03-04 15:46:27 +0900

    finish section4 on udmey learning NEXT.js at 2023/3/4 afternoon

--- nextjs-microblog/pages/index.tsx
+++ nextjs-microblog/pages/index.tsx
@@ -1,5 +1,96 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
+import utilStyle from "../styles/utils.module.css";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-    return <Layout>children</Layout>;
+    return (
+        <Layout>
+            <section className={utilStyle.headingMd}>
+                <p>
+                    私は見習いバックエンドエンジニアです/Udemyで勉強中です/好きな言語はPHPとJavascriptです
+                </p>
+            </section>
+
+            <section
+                className={`${utilStyle.headingMd} ${utilStyle.padding1px}`}
+            >
+                <h2>🧑🏻‍💻エンジニアのブログ</h2>
+                <div className={styles.grid}>
+                    <article>
+                        <Link href="/">
+                            <img
+                                className={styles.thumbnailImage}
+                                src="/images/thumbnail01.jpg"
+                                alt=""
+                            />
+                        </Link>
+                        <Link href="/">
+                            <span className={utilStyle.boldText}>
+                                SSGとSSRの使い分けの場面はいつなのか？
+                            </span>
+                        </Link>
+                        <br />
+                        <small className={utilStyle.lightText}>
+                            February 23, 2023
+                        </small>
+                    </article>
+                    <article>
+                        <Link href="/">
+                            <img
+                                className={styles.thumbnailImage}
+                                src="/images/thumbnail01.jpg"
+                                alt=""
+                            />
+                        </Link>
+                        <Link href="/">
+                            <span className={utilStyle.boldText}>
+                                SSGとSSRの使い分けの場面はいつなのか？
+                            </span>
+                        </Link>
+                        <br />
+                        <small className={utilStyle.lightText}>
+                            February 23, 2023
+                        </small>
+                    </article>
+                    <article>
+                        <Link href="/">
+                            <img
+                                className={styles.thumbnailImage}
+                                src="/images/thumbnail01.jpg"
+                                alt=""
+                            />
+                        </Link>
+                        <Link href="/">
+                            <span className={utilStyle.boldText}>
+                                SSGとSSRの使い分けの場面はいつなのか？
+                            </span>
+                        </Link>
+                        <br />
+                        <small className={utilStyle.lightText}>
+                            February 23, 2023
+                        </small>
+                    </article>
+                    <article>
+                        <Link href="/">
+                            <img
+                                className={styles.thumbnailImage}
+                                src="/images/thumbnail01.jpg"
+                                alt=""
+                            />
+                        </Link>
+                        <Link href="/">
+                            <span className={utilStyle.boldText}>
+                                SSGとSSRの使い分けの場面はいつなのか？
+                            </span>
+                        </Link>
+                        <br />
+                        <small className={utilStyle.lightText}>
+                            February 23, 2023
+                        </small>
+                    </article>
+                </div>
+            </section>
+        </Layout>
+    );
 }
